e09658b6d38930ba0c6422b0c0ee2122901bdf1f
10Sadique <jafarsjahan@gmail.com>
2023-07-18 16:21:35 +0600

    sign up form added

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,5 +1,42 @@
+import { SignUpForm } from '@/components/forms/SignUpForm';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import Link from 'next/link';
+
 const SignUp = () => {
-  return <div className="container py-2">Sign Up</div>;
+  return (
+    <div className="container flex items-center justify-center py-32">
+      <Card className="w-[450px]">
+        <CardHeader className="text-center">
+          <CardTitle className="text-2xl font-bold ">Sign Up</CardTitle>
+          <CardDescription className="font-semibold">
+            Create new account
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          {/* TODO: Social login button to be added */}
+          {/* //... */}
+          <SignUpForm />
+        </CardContent>
+
+        <CardFooter className="grid text-sm font-semibold text-muted-foreground place-items-center">
+          <p>
+            Already have an account?{' '}
+            <Link href={'/signin'} className="underline">
+              Sign In
+            </Link>
+          </p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 };
 
 export default SignUp;
---
+++ components/forms/SignUpForm.tsx
@@ -0,0 +1,119 @@
+'use client';
+
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Loader2 } from 'lucide-react';
+import { useTransition } from 'react';
+import { useRouter } from 'next/navigation';
+import { useSignUp, isClerkAPIResponseError } from '@clerk/nextjs';
+import { toast } from 'react-hot-toast';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
+const formSchema = z.object({
+  email: z
+    .string()
+    .min(3, { message: 'Email is required.' })
+    .regex(new RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$'), {
+      message: 'Enter valid email address.',
+    }),
+  password: z
+    .string()
+    .min(8, { message: 'Password at least have 6 character(s).' })
+    .regex(new RegExp('^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-zA-Z]).{8,}$'), {
+      message:
+        'Password must contain at least 1 number(s), 1 uppercase letter(s) and 1 character(s).',
+    }),
+});
+
+type SignUpSchemaType = z.infer<typeof formSchema>;
+
+export const SignUpForm = () => {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const { isLoaded, signUp } = useSignUp();
+
+  // react hook form
+  const form = useForm<SignUpSchemaType>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = (data: SignUpSchemaType) => {
+    if (!isLoaded) return;
+
+    startTransition(async () => {
+      try {
+        await signUp.create({
+          emailAddress: data.email,
+          password: data.password,
+        });
+
+        router.push('/signin');
+        toast.success('Sign up successfull.');
+      } catch (err) {
+        toast.error('Something went wrong');
+      }
+    });
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input
+                  type="email"
+                  placeholder="Enter email address"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="Enter password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button disabled={isPending} className="w-full font-semibold">
+          {isPending && <Loader2 className="w-4 h-4 mr-2 animate-spin" />}
+          Sign Up
+        </Button>
+      </form>
+    </Form>
+  );
+};
