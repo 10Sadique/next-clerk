e2ad700ee7cf173fff1b6ee324873dc0310770f3
10Sadique <jafarsjahan@gmail.com>
2023-07-18 15:03:01 +0600

    auth page created

---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,5 @@
+const SignIn = () => {
+  return <div className="container py-2">Sign In</div>;
+};
+
+export default SignIn;
---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,5 @@
+const SignUp = () => {
+  return <div className="container py-2">Sign Up</div>;
+};
+
+export default SignUp;
