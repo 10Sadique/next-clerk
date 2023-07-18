7e4b379df32cc20e061d87f593906bbd2c243132
10Sadique <jafarsjahan@gmail.com>
2023-07-18 16:21:08 +0600

    home link added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,15 @@
 import { ToggleTheme } from '@/components/ToggleTheme';
 import { NavLinks } from './NavLinks';
+import Link from 'next/link';
 
 export default function Navbar() {
   return (
     <nav className="sticky top-0 py-2 border-b">
       <div className="container flex items-center justify-between">
         <div>
-          <h1 className="text-xl font-bold">Brand</h1>
+          <Link href={'/'} className="text-xl font-bold">
+            Brand
+          </Link>
         </div>
         <div className="flex items-center space-x-6">
           <NavLinks />
