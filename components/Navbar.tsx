6f75ee2053c0433d53c1b5fedb63c73b2965d781
10Sadique <jafarsjahan@gmail.com>
2023-07-19 12:14:39 +0600

    navbar modified

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,14 +3,17 @@ import Link from 'next/link';
 import { NavLinks } from '@/components/NavLinks';
 import { ToggleTheme } from '@/components/ToggleTheme';
 import { UserDropdownMenu } from '@/components/UserDropdownMenu';
+import { siteConfig } from '@/lib/site-config';
 
 export default function Navbar() {
+  const { siteName } = siteConfig;
+
   return (
     <nav className="sticky top-0 py-2 border-b">
       <div className="container flex items-center justify-between">
         <div>
           <Link href={'/'} className="text-xl font-bold">
-            Brand
+            {siteName}
           </Link>
         </div>
         <div className="flex items-center space-x-6">
