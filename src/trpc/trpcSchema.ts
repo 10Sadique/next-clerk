b7a6d3cf28fa8bac0ebad2275193f6b55ae535b2
10Sadique <jafarsjahan@gmail.com>
2023-10-13 14:17:32 +0600

    trpc input schema added

---
+++ src/trpc/trpcSchema.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+
+export const AddProjectSchema = z.object({
+  name: z.string().min(3),
+  description: z.string().min(10),
+  gitHub: z.string().min(10),
+  live: z.string().min(10),
+  technologies: z.string().min(10),
+  mainImage: z.string().optional(),
+});
+
+export const AddUserSchema = z.object({
+  email: z
+    .string()
+    .regex(new RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$')),
+  password: z
+    .string()
+
+    .regex(new RegExp('^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-zA-Z]).{8,}$')),
+  name: z.string(),
+});
