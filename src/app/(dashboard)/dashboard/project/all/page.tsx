a2364b2dc32ff13c31907484d212ffd263f78919
10Sadique <jafarsjahan@gmail.com>
2023-10-13 15:29:48 +0600

    all projects page added

---
+++ src/app/(dashboard)/dashboard/project/all/page.tsx
@@ -0,0 +1,21 @@
+import { Heading } from '@/components/ui/header';
+import { AllProjects } from '@/components/dashboard/project/AllProjects';
+import { BackButton } from '@/components/dashboard/project/BackButton';
+
+export default function Page() {
+  return (
+    <div className="py-6 space-y-6 lg:py-8 lg:space-y-8">
+      <BackButton />
+
+      <div className="p-4 border rounded-md">
+        <Heading
+          title="Projects"
+          description={'Manage your projects.'}
+          size="sm"
+        />
+        <div className="mb-6" />
+        <AllProjects display="ALL" />
+      </div>
+    </div>
+  );
+}
