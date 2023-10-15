ee2496357e8cb2ee1d8a30a0b7fd8598deb8f3e9
10Sadique <jafarsjahan@gmail.com>
2023-10-15 18:59:04 +0600

    display all skills on progress

--- src/components/dashboard/skill/AllSkills.tsx
+++ src/components/dashboard/skill/AllSkills.tsx
@@ -1,13 +1,19 @@
 'use client';
 
 import { trpc } from '@/app/_trpc/client';
+import { buttonVariants } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
 
 type AllSkillsProps = {
   display: 'THREE' | 'ALL';
 };
 
 export const AllSkills = ({ display }: AllSkillsProps) => {
-  const { data, isLoading } = trpc.getAllSkills.useQuery();
+  const { data, isLoading } = trpc.getAllSkills.useQuery(undefined, {
+    retry: true,
+    retryDelay: 500,
+  });
 
   if (isLoading) {
     return <div className="mt-4">Loading...</div>;
@@ -23,11 +29,30 @@ export const AllSkills = ({ display }: AllSkillsProps) => {
     );
   }
 
+  const skills = display === 'THREE' ? data?.skills.slice(0, 3) : data?.skills;
+
   return (
-    <div className="mt-4">
-      {data?.skills.map((skill) => (
-        <div key={skill.id}>{skill.name}</div>
-      ))}
-    </div>
+    <>
+      <div
+        className={cn(
+          'grid grid-cols-1 gap-4 mt-4  lg:grid-cols-3 sm:grid-cols-2',
+          display === 'THREE' ? 'mb-4' : ''
+        )}
+      >
+        {skills &&
+          skills.map((skill) => <div key={skill.id}>{skill.name}</div>)}
+      </div>
+
+      {display === 'THREE' ? (
+        <div className="flex items-center justify-center">
+          <Link
+            href={'/dashboard/skill/all'}
+            className={buttonVariants({ variant: 'secondary' })}
+          >
+            See All Skills
+          </Link>
+        </div>
+      ) : null}
+    </>
   );
 };
