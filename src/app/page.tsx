63c1017852e8d75442bdb99e2a8a5275f66600bc
10Sadique <jafarsjahan@gmail.com>
2023-10-16 19:09:59 +0600

    style updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <div className="antialiased transition-all">
       <div className="isolate">
         <div
           aria-hidden="true"
